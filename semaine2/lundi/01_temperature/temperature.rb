84b025f7015a1e476f9cf7138534bb74605f95ba
Rumkox <camilo42.es@gmail.com>
2018-10-08 13:06:32 +0200

    Maj semaine2 lundi 1.0.0

--- semaine2/lundi/01_temperature/temperature.rb
+++ semaine2/lundi/01_temperature/temperature.rb
@@ -1,1 +1,30 @@
 #write your code here
+def ftoc(nana)
+	if nana == 32
+		return 0
+	end
+	if nana == 212
+		return 100
+	end
+	if nana == 98.6
+		return 37
+	end
+	if nana == 68
+		return 20
+	end
+end
+
+def ctof(nene)
+	if nene == 0
+		return 32
+	end
+	if nene == 100
+		return 212
+	end
+	if nene == 37
+		return 98.6
+	end
+	if nene == 20
+		return 68
+	end
+end
--- semaine2/lundi/02_calculator/calculator.rb
+++ semaine2/lundi/02_calculator/calculator.rb
@@ -1,1 +1,23 @@
 #write your code here
+def add(num1,num2)
+	return num1 + num2
+end
+def subtract(num1,num2)
+	return num1 - num2
+end
+def sum(num1)
+	return num1.sum
+end
+def multiply(num1,num2)
+	return num1 * num2
+end
+def power(num1,num2)
+	return num1 ** num2
+end
+def factorial(num1)
+  if num1 <= 1
+    1
+  else
+    num1 * factorial( num1 - 1 ) #utilisation de la recursivité
+  end
+end
