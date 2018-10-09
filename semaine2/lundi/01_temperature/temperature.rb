6c81335d44a06c09292046ee147365f79a1aeb8f
Rumkox <camilo42.es@gmail.com>
2018-10-09 10:54:01 +0200

    Maj semaine2 lundi 1.2.4 :Correction temp et Readme

--- semaine2/lundi/01_temperature/temperature.rb
+++ semaine2/lundi/01_temperature/temperature.rb
@@ -1,30 +1,42 @@
-#write your code here
+		#code bien fait!
 def ftoc(nana)
-	if nana == 32
-		return 0
-	end
-	if nana == 212
-		return 100
-	end
-	if nana == 98.6
-		return 37
-	end
-	if nana == 68
-		return 20
-	end
+    nana = (nana - 32) * 5 / 9
+    return nana.to_i
 end
 
-def ctof(nene)
-	if nene == 0
-		return 32
-	end
-	if nene == 100
-		return 212
-	end
-	if nene == 37
-		return 98.6
-	end
-	if nene == 20
-		return 68
-	end
+def ctof(nana)
+    nana = (nana.to_f * 9 / 5) + 32
+    return nana.to_f
 end
+
+
+		#Code mal fait! ? pas de calcul
+#def ftoc(nana)
+#	if nana == 32
+#		return 0
+#	end
+#	if nana == 212
+#		return 100
+#	end
+#	if nana == 98.6
+#		return 37
+#	end
+#	if nana == 68
+#		return 20
+#	end
+#end
+
+#def ctof(nene)
+#	if nene == 0
+#		return 32
+#	end
+#	if nene == 100
+#		return 212
+#	end
+#	if nene == 37
+#		return 98.6
+#	end
+#	if nene == 20
+#		return 68
+#	end
+#end
--- semaine2/lundi/README.md
+++ semaine2/lundi/README.md
@@ -2,6 +2,9 @@
 <br/>
 <h2>Semaine 2 - Lundi</h2>
 <h4>Bonjour!!</h4>
+<h4>je dois corriger les exos 01_temperature,03_basics,06_pig_latin.</h4>
+<h5>Fini! <a href="https://github.com/rumkox/TheHackingProject2018/blob/master/semaine2/lundi/01_temperature/temperature.rb">01_temperature</a></h5>
+<br/>
 <h5>co-programmeur = Antoine -> Slack = @tonoutt</h5>
 <br/>
 <h3>Voici mon<a href="https://twitter.com/Camilo42Es?lang=fr"> Twiiter</a><br/>
