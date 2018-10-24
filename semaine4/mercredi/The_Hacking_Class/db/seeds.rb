bd789f91f63587b047a74abbdebef9799f2d29be
Rumkox <camilo42.es@gmail.com>
2018-10-24 21:46:04 +0200

    Maj semaine 4 mercredi 1.0.2 :Fin The_Hacking_Class

--- semaine4/mercredi/The_Hacking_Class/db/seeds.rb
+++ semaine4/mercredi/The_Hacking_Class/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+#require 'faker'
+
+cours = Course.create(title: "Mathematiques")
+
+10.times do
+  eleve = Eleve.create!(name: Faker::FunnyName.name, course_id: Faker::Number.between(1, 1))
+end
+
+#10.times do
+#  user = User.create!(first_name: Faker::Name.first_name,last_name: Faker::Name.last_name, email: Faker::Internet.email)
+#end
